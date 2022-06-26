f6d93a8d7865de325131bffa9993657ec3e5d5da
SesisnandoLRNeto <nandolrneto@gmail.com>
2022-06-26 15:07:32 -0400

    Class 04: Finish application with Subscriber register

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,8 @@
 import { format, isPast } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { CheckCircle, Lock } from 'phosphor-react';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
 
 interface LessonProps {
   title: string;
@@ -10,20 +12,32 @@ interface LessonProps {
 }
 
 export function Lesson({ title, slug, availableAt, typeClass }: LessonProps) {
+  const { slug: slugActived } = useParams<{ slug: string }>();
+
   const isLessonAvailable = isPast(availableAt);
   const availableDataFormatted = format(
     availableAt,
-    "EEEE' • 'd' de 'MMMM' • 'k'h'mm",
-    { locale: ptBR }
+    "EEEE' • 'd' de 'MMMM' • 'k'h'mm"
+    // { locale: ptBR }
   );
 
+  const isActiveLesson = slugActived === slug;
+
   return (
-    <a href='#'>
+    <Link to={`/event/lesson/${slug}`} className='group'>
       <span className='text-gray-300'>{availableDataFormatted}</span>
-      <div className='rounded border border-gray-500 p-4 mt-2'>
+      <div
+        className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${
+          isActiveLesson && 'bg-green-500'
+        }`}
+      >
         <header className='flex items-center justify-between'>
           {isLessonAvailable ? (
-            <span className='flex items-center gap-2 text-sm text-blue-500 font-medium'>
+            <span
+              className={`flex items-center gap-2 text-smfont-medium ${
+                isActiveLesson && 'text-white'
+              } ${!isActiveLesson && ' text-blue-500 '}`}
+            >
               <CheckCircle size={20} />
               Content available
             </span>
@@ -33,12 +47,22 @@ export function Lesson({ title, slug, availableAt, typeClass }: LessonProps) {
               Coming soon
             </span>
           )}
-          <span className='text-xs rounded px-2 py-[2px] text-white border border-green-300 font-bold'>
+          <span
+            className={`text-xs rounded px-2 py-[2px] text-white border font-bold ${
+              isActiveLesson && 'border-white'
+            } ${!isActiveLesson && '  border-green-300 '}}`}
+          >
             {typeClass === 'live' ? 'Live' : 'Practice Class'}
           </span>
         </header>
-        <strong className='text-gray-200 mt-5 block'>{title}</strong>
+        <strong
+          className={`mt-5 block ${isActiveLesson && 'text-white'} ${
+            !isActiveLesson && 'text-gray-200'
+          }`}
+        >
+          {title}
+        </strong>
       </div>
-    </a>
+    </Link>
   );
 }
---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,90 @@
+import { gql, useMutation } from '@apollo/client';
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Logo } from '../components/Logo';
+
+const CREATE_SUBSCRIBE_MUTATION = gql`
+  mutation CreateSubscriber($name: String!, $email: String!) {
+    createSubscriber(data: { name: $name, email: $email }) {
+      id
+    }
+  }
+`;
+
+export function Subscribe() {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+  });
+
+  const [createSubscriber, { loading }] = useMutation(
+    CREATE_SUBSCRIBE_MUTATION
+  ); // To get data result, just increase , { data } into desestructuring declaration
+
+  async function handleSubscribe(event: FormEvent) {
+    event.preventDefault();
+
+    await createSubscriber({
+      variables: form,
+    });
+
+    navigate('/event');
+  }
+
+  return (
+    <div className='min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center'>
+      <div className='w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto'>
+        <div className='mx-w-[640px]'>
+          <Logo />
+          <h1 className='mt-8 text-[2.5rem] leading-tight'>
+            Build a {''}
+            <strong className='text-blue-500'>Complete Application</strong>,
+            from scratch with
+            <strong className='text-blue-500'> React JS</strong>
+          </h1>
+          <p className='mt-4 text-gray-200 leading-relaxed'>
+            In just one week you will master in practice one of the most used
+            with high demand to access the best opportunities in the market.
+          </p>
+        </div>
+
+        <div className='p-8 bg-gray-700 border border-gray-500 rounded'>
+          <strong className='text-2xl mb-6 block'>Sign up free</strong>
+          <form
+            action=''
+            onSubmit={handleSubscribe}
+            className='flex flex-col gap-2 w-full'
+          >
+            <input
+              className='bg-gray-900 rounded px-5 h-14'
+              type='text'
+              placeholder='Your complete name'
+              onChange={(e) => setForm({ ...form, name: e.target.value })}
+            />
+            <input
+              className='bg-gray-900 rounded px-5 h-14'
+              type='email'
+              placeholder='Type your e-mail'
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+
+            <button
+              className='mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50'
+              type='submit'
+              disabled={loading}
+            >
+              Guarantee my spot
+            </button>
+          </form>
+        </div>
+      </div>
+      <img
+        src='/src/assets/code-mockup.png'
+        className='mt-10'
+        alt='image mockup'
+      />
+    </div>
+  );
+}
